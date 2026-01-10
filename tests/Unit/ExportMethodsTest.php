e8ba51e9456fe5933c83bcb05803e09926b667fc
daniel <skyrimdaniel11@gmail.com>
2026-01-10 00:09:58 -0300

    test: to export to json method

--- tests/Unit/ExportMethodsTest.php
+++ tests/Unit/ExportMethodsTest.php
@@ -20,3 +20,12 @@ it('can convert a DTO to an array', function () {
     // $arrayWithData must contain an email key equals to alvarez@alvarez.com
     expect($arrayWithData['email'])->toBe('alvarez@alvarez.com');
 });
+
+it('can converto a DTO to a JSON', function () {
+    // create a patient DTO
+    $patientDTO = new PatientDTO(email: 'alvarez@alvarez.com');
+    // convert this DTO in a JSON
+    $json = $patientDTO->toJson();
+    expect($json)->toBeJson();
+    expect($json)->toBe('{"email":"alvarez@alvarez.com"}');
+});
