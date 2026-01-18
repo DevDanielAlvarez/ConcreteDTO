618eec3925269da612622803c98f5e2c2ec03dce
DevDanielAlvarez <DevDanielAlvarez@users.noreply.github.com>
2026-01-18 15:47:39 +0000

    deploy: 28b6cc846da441a9dbfbc0cc7424119438a9d6d4

---
+++ assets/index.md.D3X5ugtp.lean.js
@@ -0,0 +1,1 @@
+import{_ as i,c as a,o as n,ag as l}from"./chunks/framework.BjBghX3T.js";const g=JSON.parse('{"title":"ConcreteDTO","description":"","frontmatter":{},"headers":[],"relativePath":"index.md","filePath":"index.md"}'),t={name:"index.md"};function h(p,s,e,k,r,d){return n(),a("div",null,[...s[0]||(s[0]=[l("",11)])])}const o=i(t,[["render",h]]);export{g as __pageData,o as default};
